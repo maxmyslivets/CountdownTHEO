f7d3f43ce008909816ddbabcc9f8b7afb9cf89de
maxmyslivets <37196527+maxmyslivets@users.noreply.github.com>
2020-11-08 21:23:36 +0300

    Обработка каталога точек
    Формирование списка пикетов. Запись каталога точек хода и пикетов в виде (№ хода, станция, обнуление, пикеты)

--- moduls/points_to_stantions.py
+++ moduls/points_to_stantions.py
@@ -35,7 +35,61 @@ while True:
             namber += 1
         elif a == '1':
             break
+        else:
+            print("Введено неверное значение... Начните ввод всех точек заново.")
+            lines = []
+            break
 
 for i in range(len(lines)):
     print("Ход №", i+1, ":", lines[i])
 
+text_st_0_p = ""
+# Ввод пикетов для станций
+for i in range(len(lines)):
+    k = i + 1
+    for st in lines[i]:
+        #FIXME: Добавить проверку на ввод несуществующих пикетов
+        line = []
+        print("Ввод пикетов для хода № %s. Станции %s" % (i+1, lines[i]))
+        line.append(k)
+        line.append(st)
+        picketList = input("Введите точку обнуления и пикеты для станции %s:" % st).split(",")
+        for p in picketList:
+            line.append(p)
+        lineText = ""
+        for l in line:
+            lineText += str(l) + ","
+        text_st_0_p += "\n" + str(lineText)[0:-1]
+
+# Запись (ход, станция, обнуление, пикеты) в файл st_0_p.txt
+with open("st-0-p.txt", "w") as st_0_p:
+    st_0_p.write(text_st_0_p[1:])
+
+print(
+    """
+    Произведена запись вида:
+        № хода, cтанция, точка обнуления, пикеты...
+    в файл st_0_p.txt
+    """
+)
+
+# Проверка неиспользуемых точек из каталога
+with open("st-0-p.txt", "r") as st_0_p:
+    st_0_p_Catalog = (st_0_p.read().split("\n"))
+    catalogPoints = []
+    for i in range(len(st_0_p_Catalog)):
+        stringPoint = st_0_p_Catalog[i].split(",")
+        del stringPoint[0]
+        for j in stringPoint:
+            catalogPoints.append(j)
+        
+    catalogPointsNo = []
+    for i in pointName:
+        if i not in catalogPoints:
+            catalogPointsNo.append(i)
+    
+    if len(catalogPointsNo) != 0:
+        print("В обработку не попали точки: ", catalogPointsNo)
+        input("Нажмите любю клавишу для завершения.")
+    else:
+        input("Все точки были выбраны в обработку. Нажмите любю клавишу для завершения.")
